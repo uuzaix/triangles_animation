237b9c32809802bb6154719809aa827926ffb386
uuzaix <larisa.dolgova@gmail.com>
2018-04-02 00:15:02 +0200

    Generate divs for all triangles

---
+++ script.js
@@ -0,0 +1,10 @@
+window.onload = function() {
+  var htmlElements = ''
+  for (var i = 1; i < 85; i++) {
+    htmlElements += '<div class=t' + i + '></div>'
+  }
+  var container = document.getElementById("canvas");
+  container.innerHTML = htmlElements;
+}
+
+
